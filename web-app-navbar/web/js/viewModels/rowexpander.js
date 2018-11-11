63d64d49b9120a6720de4d9435a58316596ef14b
Pedro Pascoal <pedro.pascoal.4@gmail.com>
2018-11-11 19:10:01 +0000

    api call added

--- web-app-navbar/web/js/viewModels/rowexpander.js
+++ web-app-navbar/web/js/viewModels/rowexpander.js
@@ -14,23 +14,23 @@ require(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout',
                 'columns': ['id','resource','start','end']
             };
             self.dataSource = ko.observable();
-            $( async function()
-                {
-                    //let data = await userAction();
-                    let data = [{"attr":{"end":"10/1/2014","id":"t1","name":"Task 1","resource":"Chadwick","start":"1/1/2014"},"children":[{"attr":{"end":"3/1/2014","id":"t1:1","name":"Task 1-1","resource":"Chris","start":"1/1/2014"},"children":[{"attr":{"end":"2/1/2014","id":"t1:1:1","name":"Task 1-1-1","resource":"Henry","start":"1/1/2014"}},{"attr":{"end":"3/1/2014","id":"t1:1:2","name":"Task 1-1-2","resource":"Victor","start":"2/1/2014"}}]},{"attr":{"end":"6/1/2014","id":"t1:2","name":"Task 1-2","resource":"Jim","start":"3/1/2014"},"children":[{"attr":{"end":"5/1/2014","id":"t1:2:1","name":"Task 1-2-1","resource":"Jay","start":"3/1/2014"}},{"attr":{"end":"6/1/2014","id":"t1:2:2","name":"Task 1-2-2","resource":"Karin","start":"5/1/2014"}}]},{"attr":{"end":"8/1/2014","id":"t1:3","name":"Task 1-3","resource":"Chadwick","start":"6/1/2014"}},{"attr":{"end":"10/1/2014","id":"t1:4","name":"Task 1-4","resource":"Chris","start":"8/1/2014"}}]}];
-                    console.log("data -> " + JSON.stringify(data, null, 2));
-                    self.dataSource(new oj.FlattenedTreeDataGridDataSource(new oj.JsonTreeDataSource(data), options));
-                }
-            );
-
-            // $(
-            //    async function(data)
+            // $( async function()
             //     {
-            //         data = await userAction();
-            //         console.log(JSON.stringify(data, null, 2));
+            //         //let data = await userAction();
+            //         let data = [{"attr":{"end":"10/1/2014","id":"t1","name":"Task 1","resource":"Chadwick","start":"1/1/2014"},"children":[{"attr":{"end":"3/1/2014","id":"t1:1","name":"Task 1-1","resource":"Chris","start":"1/1/2014"},"children":[{"attr":{"end":"2/1/2014","id":"t1:1:1","name":"Task 1-1-1","resource":"Henry","start":"1/1/2014"}},{"attr":{"end":"3/1/2014","id":"t1:1:2","name":"Task 1-1-2","resource":"Victor","start":"2/1/2014"}}]},{"attr":{"end":"6/1/2014","id":"t1:2","name":"Task 1-2","resource":"Jim","start":"3/1/2014"},"children":[{"attr":{"end":"5/1/2014","id":"t1:2:1","name":"Task 1-2-1","resource":"Jay","start":"3/1/2014"}},{"attr":{"end":"6/1/2014","id":"t1:2:2","name":"Task 1-2-2","resource":"Karin","start":"5/1/2014"}}]},{"attr":{"end":"8/1/2014","id":"t1:3","name":"Task 1-3","resource":"Chadwick","start":"6/1/2014"}},{"attr":{"end":"10/1/2014","id":"t1:4","name":"Task 1-4","resource":"Chris","start":"8/1/2014"}}]}];
+            //         console.log("data -> " + JSON.stringify(data, null, 2));
             //         self.dataSource(new oj.FlattenedTreeDataGridDataSource(new oj.JsonTreeDataSource(data), options));
             //     }
             // );
+
+            $(
+               async function(data)
+                {
+                    data = await userAction();
+                    console.log(JSON.stringify(data, null, 2));
+                    self.dataSource(new oj.FlattenedTreeDataGridDataSource(new oj.JsonTreeDataSource(data), options));
+                }
+            );
 
             // $.getJSON( "https://crossorigin.me/http://127.0.0.1:5002/employees",
             //     function(data)
