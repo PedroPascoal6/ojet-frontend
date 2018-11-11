e4f7c325a7caf9a6206ef9e3866f3349f7f9ae05
Pedro Pascoal <pedro.pascoal.4@gmail.com>
2018-11-11 18:10:28 +0000

    index add app

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 var http = require ('http')
 
 http.createServer(function (request, response ){
     response.writeHead(200, {"Content-Type":"text/plain"})
     response.end("Hello World\n")
+    app.use(express.static("web-app-navbar/web"))
+    app.listen(process.env.PORT || 3007);
 }).listen(process.env.PORT)
 // app.use(express.static("web-app-navbar/web"))
 // app.listen(process.env.PORT || 3007);
